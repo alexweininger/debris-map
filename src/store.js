e87a38ef661f362c73d4b4ec46495e946da38efd
alexweininger <alex.weininger@live.com>
2021-04-15 22:59:05 -0700

    able to import image files

--- src/store.js
+++ src/store.js
@@ -4,7 +4,22 @@ export const useStore = create(set => ({
     typeFilter: [],
     dateFilter: [],
     points: [],
-    setPoints: (points) => set(state => ({ points })),
+    setPoints: (points) => set(state => ({ points: [...state.points, ...points] })),
     setTypeFilter: (filter) => set(state => ({ typeFilter: filter })),
-    setDateFilter: (filter) => set(state => ({ dateFilter: filter }))
+    setDateFilter: (filter) => set(state => ({ dateFilter: filter })),
+    complete: [],
+    removeComplete: (point) => set(state => {
+        let newC = Array.from(state.complete);
+        newC = newC.filter((p) => p !== point['Litter']);
+        return { complete: newC };
+    }),
+    addComplete: (point) => set(state => {
+        const newC = Array.from(state.complete);
+        newC.push(point['Litter']);
+        return { complete: newC };
+    }),
+    mapLoading: 'idle',
+    setMapLoading: (value) => set(state => ({ mapLoading: value })),
+    showPoints: false,
+    setShowPoints: (val) => set(state => ({ showPoints: val }))
 }))
